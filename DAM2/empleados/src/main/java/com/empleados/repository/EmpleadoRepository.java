43478699d7d9a8f3f557fa3c9ccd038933e0abdd
antoniobenitez01 <abenrod@g.educaand.es>
2025-12-01 20:57:51 +0100

    General Commit

--- DAM2/empleados/src/main/java/com/empleados/repository/EmpleadoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.empleados.repository;
-
-
-import com.empleados.entity.Empleados;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface EmpleadoRepository extends JpaRepository<Empleados, Integer> {
-}  
